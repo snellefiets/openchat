b69fdb4e0c2348fc981172ade4544a676084daad
danny liebmann <dliebmann@it-economics.de>
2022-03-04 10:55:52 +0100

    implements StatusApi Controller

---
+++ src/main/java/de/ite/openchat/api/StatusApi.java
@@ -0,0 +1,16 @@
+package de.ite.openchat.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status/")
+public class StatusApi {
+
+    @GetMapping
+    public ResponseEntity<String> getStatus() {
+        return ResponseEntity.ok("{\"status\": \"Application is running!\"}");
+    }
+}
--- src/test/java/de/ite/openchat/api/StatusApiIT.java
+++ src/test/java/de/ite/openchat/api/StatusApiIT.java
@@ -1,7 +1,6 @@
 package de.ite.openchat.api;
 
 import lombok.SneakyThrows;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
