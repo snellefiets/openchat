45af6e367faf4a1e29746cc45e0a0521cdfcbaf6
danny liebmann <dliebmann@it-economics.de>
2022-03-04 10:06:12 +0100

    initial commit

---
+++ src/main/java/de/ite/openchat/OpenchatApplication.java
@@ -0,0 +1,13 @@
+package de.ite.openchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ite/openchat/OpenchatApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ite.openchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
