a36ae7f40b58dc30be7e1719ea7ec919f4e0d4e0
danny liebmann <dliebmann@it-economics.de>
2022-03-09 17:49:23 +0100

    UserRegistration: UserService implements createUser

--- src/main/java/de/ite/openchat/domain/UserService.java
+++ src/main/java/de/ite/openchat/domain/UserService.java
@@ -1,5 +1,6 @@
 package de.ite.openchat.domain;
 
+import de.ite.openchat.repository.UserDbo;
 import de.ite.openchat.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class UserService {
     }
 
     public User createUser(RegistrationData registrationData) throws UserAlreadyInUseException {
-        throw new UnsupportedOperationException();
+        final String userId = userRepository.save(UserDbo.builder().name(registrationData.getUsername()).password(registrationData.getPassword()).about(registrationData.getAbout()).build());
+        return User.builder().id(userId).name(registrationData.getUsername()).password(registrationData.getPassword()).about(registrationData.getAbout()).build();
     }
 }
