7abcb2ee999cdcf584600792ad361fd8699260fe
danny liebmann <dliebmann@it-economics.de>
2022-03-10 08:56:44 +0100

    UserRegistration: refactors UserRepositoryImplTest

--- src/test/java/de/ite/openchat/repository/UserRepositoryImplTest.java
+++ src/test/java/de/ite/openchat/repository/UserRepositoryImplTest.java
@@ -7,33 +7,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UserRepositoryImplTest {
 
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String PASSWORD = "password";
+    private static final String ABOUT = "about";
     private UserRepositoryImpl sut;
+    private UserDbo user;
 
     @BeforeEach
     void setUp() {
         sut = new UserRepositoryImpl();
+        user = UserDbo.builder().id(ID).name(NAME).password(PASSWORD).about(ABOUT).build();
     }
 
     @Test
     void should_find_an_user_existence() {
-        sut.userList.add(UserDbo.builder().name("name").password("password").about("about").build());
-        assertThat(sut.existsByName("name")).isTrue();
+        sut.userList.add(user);
+        assertThat(sut.existsByName(NAME)).isTrue();
     }
 
     @Test
     void should_NOT_find_an_user_existence() {
-        assertThat(sut.existsByName("user")).isFalse();
+        assertThat(sut.existsByName(NAME)).isFalse();
     }
 
     @Test
     void should_find_an_user_by_id() {
-        final UserDbo user = UserDbo.builder().id("id").name("name").password("password").about("about").build();
         sut.userList.add(user);
-        assertThat(sut.findById("id")).isEqualTo(user);
+        assertThat(sut.findById(ID)).isEqualTo(user);
     }
 
     @Test
     void should_NOT_find_an_user_by_id() {
-        assertThat(sut.findById("id")).isNull();
+        assertThat(sut.findById(ID)).isNull();
     }
 }
